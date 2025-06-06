08faf41231c21e53fb55b9bf404064a5f2e07b99
Hunter Yeagley <hunter.c.yeagley@outlook.com>
2025-06-06 09:42:03 -0500

    Update models.ts

--- lib/types/models.ts
+++ lib/types/models.ts
@@ -13,6 +13,12 @@ export const models: Model[] = [
     providerId: 'openai'
   },
   {
+    id: 'o4-mini-2025-04-16',
+    name: 'GPT-o4',
+    provider: 'OpenAI',
+    providerId: 'openai'
+  },
+  {
     id: process.env.NEXT_PUBLIC_OPENAI_COMPATIBLE_MODEL || 'undefined',
     name: process.env.NEXT_PUBLIC_OPENAI_COMPATIBLE_MODEL || 'Undefined',
     provider: 'Local Models (Hunter Only)',
